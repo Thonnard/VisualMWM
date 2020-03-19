1d650d77a4ed67b1ccbcdd37715a78ebcdae5ab2
David Thonnard <thonnard.david@gmail.com>
2020-03-19 16:20:52 +0100

    document data

---
+++ R/track_data-data.R
@@ -0,0 +1,26 @@
+#' Track data of 4 trials in a Morris water maze experiment
+#'
+#' @name track_data
+#'
+#' @docType data
+#'
+#' @usage data(track_data)
+#'
+#' @format A data frame with 81889 rows and 7 columns.
+#' \describe{
+#' \itemize{
+#'   \item Time: Timestamp
+#'   \item x: x coordinate in Morris water maze setup
+#'   \item y: y coordinate in Morris water maze setup
+#'   \item Animal: Animal ID
+#'   \item Day: Day of experiment (numeric)
+#'   \item Trial: Trial number
+#'   \item Group: Group information (NA)
+#' }
+#' }
+#'
+#' @keywords datasets Morris water maze mwm track
+#'
+#' @examples
+#' data(track_data)
+"track_data"
