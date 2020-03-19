9887e510ef2d6d0e7ef640073432542532ecf912
David Thonnard <thonnard.david@gmail.com>
2020-03-19 14:09:12 +0100

    update globals

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,2 @@
-utils::globalVariables(c("circles", "x0", "r", "circle","x_scaled","y_scaled","Time","DistanceToTarget","Trial","Swimspeed","."))
+utils::globalVariables(c("circles", "x0", "r", "circle","x_scaled","y_scaled","Time",
+                         "DistanceToTarget","Trial","Swimspeed","."))
