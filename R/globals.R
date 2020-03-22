40cf5ebe2db65b391c3704095e80aa6a6b04135f
David Thonnard <thonnard.david@gmail.com>
2020-03-22 19:41:22 +0100

    update globals

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,3 @@
-utils::globalVariables(c("circles", "x0", "r", "circle","x_scaled","y_scaled","Time",
-                         "DistanceToTarget","Trial","Swimspeed","."))
+utils::globalVariables(c("circles", "x0", "r", "circle", "x_scaled", "y_scaled", "Time",
+                         "DistanceToTarget", "Trial","Swimspeed", ".", "Mean_Time_Quadrant",
+                         "Quadrant", "Time_Quadrant", "Time_bin", "ease_aes", "transition_states"))
