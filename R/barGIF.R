f7e48d37c8c47a1deaff61c1aa06ea56d5a0bf13
David Thonnard <thonnard.david@gmail.com>
2020-03-27 20:27:19 +0100

    try fix function call

--- R/barGIF.R
+++ R/barGIF.R
@@ -128,7 +128,7 @@ barGIF <- function(data, id, day, trial,
     scale_x_discrete(name="Quadrant", breaks=c("TQ","OQ","AL","AR"), labels=c("Target", "Opposite", "Adjacent\nLeft", "Adjacent\nRight")) +
     ylab("Time in quadrant (%)") +
     # transition
-    transition_states(Time_bin, transition_length = 1) +
+    gganimate::transition_states(Time_bin, transition_length = 1) +
     ease_aes('sine-in-out') +
     theme_classic() +
     theme(legend.position = "none", axis.text = element_text(size=12, face = "bold", colour = "black"),
